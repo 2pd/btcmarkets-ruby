5a72b214e7343233c67ae8509266c6415f3f98de
Liang Shi <liang.shi@protonmail.com>
2019-11-09 21:36:22 +1100

    bump version

--- lib/btcmarkets/version.rb
+++ lib/btcmarkets/version.rb
@@ -1,3 +1,3 @@
 module BTCMarkets
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- spec/btcmarkets_spec.rb
+++ spec/btcmarkets_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe BTCMarkets do
   it "has a version number" do
-    expect(BTCMarkets::VERSION).to eq "0.1.0"
+    expect(BTCMarkets::VERSION).to eq "0.1.1"
   end
 end
